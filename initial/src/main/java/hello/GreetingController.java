625f514e91475d1ded66f8f85c1f6b0e5f02bdee
Juan F Gonzalez <juano_gonza@hotmail.com>
2016-10-12 17:22:21 -0500

    Initial
    Initial exercise completed with sources

---
+++ initial/src/main/java/hello/GreetingController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author 2081391
+ */
+@Controller
+public class GreetingController {
+    
+    
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception{
+        Thread.sleep(1000);
+        return new Greeting("Hello" + message.getName() + "!");
+    }
+}
